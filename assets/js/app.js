e0ba4b3ac27e8b871499ccc3a545ee72800b7e49
8 <lucinda.souchet@outlook.com>
2021-09-16 01:39:17 +0200

    feat: add click function on previous and next buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('AsterixApp', []);
 
 app.controller('ComicController', function($scope) {
-    $scope.comicsCount = 0;
+    $scope.comicCount = 0;
 
     $scope.comics = [
         "assets/images/comics/asterix.png",
@@ -9,6 +9,21 @@ app.controller('ComicController', function($scope) {
         "assets/images/comics/deadpool.png",
         "assets/images/comics/luky_luke.png",
         "assets/images/comics/persepolis.png",
-        "assets/images/comics/tintin.png",
+        "assets/images/comics/tintin.png"
     ]
+
+    $scope.previous = function() {
+        if ($scope.comicCount > 0)
+            $scope.comicCount = $scope.comicCount - 1;
+        else
+            $scope.comicCount = 0;
+    }
+
+    $scope.next = function() {
+        if ($scope.comicCount < 5)
+            $scope.comicCount = $scope.comicCount + 1;
+        else
+            $scope.comicCount = 5;
+    }
+
 });
