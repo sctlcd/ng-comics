ebd7a0df89015f11e6965b41b5bc024379d71ba1
8 <lucinda.souchet@outlook.com>
2021-09-16 01:05:03 +0200

    feat: add and display images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,14 @@
 var app = angular.module('AsterixApp', []);
 
 app.controller('ComicController', function($scope) {
+    $scope.comicsCount = 0;
 
+    $scope.comics = [
+        "assets/images/comics/asterix.png",
+        "assets/images/comics/batman.png",
+        "assets/images/comics/deadpool.png",
+        "assets/images/comics/luky_luke.png",
+        "assets/images/comics/persepolis.png",
+        "assets/images/comics/tintin.png",
+    ]
 });
